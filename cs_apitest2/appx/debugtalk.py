b40d0309a97b506dd27b4532189cffb4f4578142
liulang <newlang@126.com>
2020-12-27 11:41:00 +0800

    提交

---
+++ cs_apitest2/appx/debugtalk.py
@@ -0,0 +1,28 @@
+'''
+1。可选
+2. 存在， 所在目录为项目的根目录
+3. 不存在时， 当前目录，为根目录
+4. yaml文件中， csv 文件路径，都相对于根目录
+'''
+
+import hashlib
+import hmac
+import random
+import string
+
+SECRET_KEY = "DebugTalk"
+
+def gen_random_string(str_len):
+    random_char_list = []
+    for _ in range(str_len):
+        random_char = random.choice(string.ascii_letters + string.digits)
+        random_char_list.append(random_char)
+
+    random_string = ''.join(random_char_list)
+    return random_string
+
+def get_sign(*args):
+    content = ''.join(args).encode('ascii')
+    sign_key = SECRET_KEY.encode('ascii')
+    sign = hmac.new(sign_key, content, hashlib.sha1).hexdigest()
+    return sign
